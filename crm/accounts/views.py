dfffe5baeef459f93a947dd020425a6107e69a23
Britney Nguyen <nguyen26.bn@gmail.com>
2022-11-22 22:18:37 -0800

    home page created & footer template

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    return render(request, 'accounts/home.html')
 
 def about(request):
     return render(request, 'accounts/about.html')
